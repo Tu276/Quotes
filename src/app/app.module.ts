948da0ab433982e2745e703af49e1cae218587ea
Tu276 <tu276nathan@gmail.com>
2020-09-19 18:22:49 +0300

    add custom pipe date-count

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { StrikethroughDirective } from './strikethrough.directive';
     QuoteComponent,
     QuoteDetailComponent,
     QuoteFormComponent,
-    StrikethroughDirective
+    StrikethroughDirective,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
